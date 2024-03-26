7c52a5529cfa4000fd8836cbfcc181f1cf1012e6
Martin <m@mrtn.li>
2024-03-26 13:30:47 +0100

    Updated dock items

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,6 +3,7 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/Applications/Spark Desktop.app"
+dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Warp.app"
@@ -10,6 +11,7 @@ dockutil --no-restart --add "/Applications/Sublime Merge.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Xcode.app"
 dockutil --no-restart --add "/Applications/Figma.app"
+dockutil --no-restart --add "/Applications/Obsidian.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 
 killall Dock
